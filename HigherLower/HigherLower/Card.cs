ca6bedbf4a2a5f64f5d2c5291ef23eca00e18e3c
Luke McDowell <luke.mcdowell@pwc.com>
2022-07-11 15:32:42 +0100

    Adding card and deck classes

---
+++ HigherLower/HigherLower/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace HigherLower
+{
+    public class Card
+    {
+        public string suit { get; set; }
+        public string rank { get; set; }
+
+        public Card(string cardSuit, string cardRank)
+        {
+            suit = cardSuit;
+            rank = cardRank;
+        }
+    }
+}
