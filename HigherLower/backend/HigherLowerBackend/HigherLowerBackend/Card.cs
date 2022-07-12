febe1072cc940d9fc512e818543ee01e6cffbb00
Luke McDowell <luke.mcdowell@pwc.com>
2022-07-12 11:55:13 +0100

    Updating project template with working api

---
+++ HigherLower/backend/HigherLowerBackend/HigherLowerBackend/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace HigherLowerBackend
+{
+    public class Card
+    {
+        public string suit { get; set; }
+        public string rank { get; set; }
+
+        public Card(string cardSuit, string cardRank)
+        {
+            suit = cardSuit;
+            rank = cardRank;
+        }
+    }
+}
---
+++ HigherLower/backend/HigherLowerBackend/HigherLowerBackend/Controllers/CardGameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HigherLowerBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardGameController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+            Deck deck = new Deck();
+            IEnumerable<Card> cards = deck.cards;
+            return cards;
+        }
+    }
+}
